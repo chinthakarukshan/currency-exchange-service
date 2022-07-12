5b882a8f2e40119e399fa50404cd86870f6be822
chinthaka <chinthakarukshan@gmail.com>
2022-07-12 23:48:41 +0530

    Initial commit of the project

---
+++ src/test/java/com/brushup/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brushup.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
