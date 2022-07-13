3366e6cb472c59b0fe809393b93bccbb3ca0e754
chinthaka <chinthakarukshan@gmail.com>
2022-07-13 23:35:41 +0530

    Configured currency exchange service to return the environment details

--- src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.brushup.microservices.currencyexchangeservice.controller;
 
 import com.brushup.microservices.currencyexchangeservice.dto.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,14 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchange(1000,"USD","LKR", BigDecimal.valueOf(395));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000,"USD","LKR", BigDecimal.valueOf(395));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
