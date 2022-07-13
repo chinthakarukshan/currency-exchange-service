5c5683b100b52e581f33e143d11546902c6a552f
chinthaka <chinthakarukshan@gmail.com>
2022-07-13 23:23:53 +0530

    Added new currency exchange service

--- src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,4 +1,17 @@
 package com.brushup.microservices.currencyexchangeservice.controller;
 
+import com.brushup.microservices.currencyexchangeservice.dto.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new CurrencyExchange(1000,"USD","LKR", BigDecimal.valueOf(395));
+    }
 }
