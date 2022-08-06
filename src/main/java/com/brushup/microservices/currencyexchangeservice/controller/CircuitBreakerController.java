ca5275a5f79b044eb0850c23fef142327af40914
chinthaka <chinthakarukshan@gmail.com>
2022-08-07 00:02:02 +0530

    Setup Sample API to check circuit breaker

--- src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,4 +1,14 @@
 package com.brushup.microservices.currencyexchangeservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CircuitBreakerController {
+
+    @GetMapping("/sample-api")
+    public String sampleAPI() {
+        return "Sample API";
+    }
+
 }
