c677c806491b8f5703b820e767609c509b4a4fda
chinthaka <chinthakarukshan@gmail.com>
2022-08-07 00:10:33 +0530

    Added default retry capability using resilience4j

--- src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,14 +1,25 @@
 package com.brushup.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    @Retry(name = "default")
     public String sampleAPI() {
-        return "Sample API";
+        logger.info("Sample API Call received");
+        ResponseEntity<String> responseBody =  new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
+        return responseBody.getBody();
     }
 
 }
