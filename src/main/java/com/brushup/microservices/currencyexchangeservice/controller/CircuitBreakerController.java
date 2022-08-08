d36c55be2a4b91a295771301044ef92488b2ddd1
chinthaka <chinthakarukshan@gmail.com>
2022-08-09 00:20:10 +0530

    Enabled bulk head for the sample api

--- src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/brushup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.brushup.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -19,7 +20,8 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-    @RateLimiter(name = "default")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name="default")
     public String sampleAPI() {
         logger.info("Sample API Call received");
         //ResponseEntity<String> responseBody =  new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
